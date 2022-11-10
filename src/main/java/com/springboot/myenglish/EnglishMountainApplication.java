6ffe43d0c6861393a0d6125644c7862f81bf68e1
nulideManh <manh.nv1005@gmail.com>
2022-11-10 16:15:58 +0700

    first commit

---
+++ src/main/java/com/springboot/myenglish/EnglishMountainApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.myenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishMountainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishMountainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/myenglish/EnglishMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.myenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishMountainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
